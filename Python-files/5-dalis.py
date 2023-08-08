46b5453f2a7e2f7feeb8dca93eca90a1b959f941
Žygimantas Girdauskas <zygiss.g@gmail.com>
2023-08-08 03:20:38 +0300

    update commit

--- Python-files/5-dalis.py
+++ Python-files/5-dalis.py
@@ -14,16 +14,20 @@ most_countries = countries * 0.75
 
 # Group data by the WEO Subject Code and count how many countries have data in 2015
 df_count = df.groupby("WEO Subject Code")[2015].count()
-print(df_count)
 
 # Filter dataframe to only include data fields that are present in most of the countries
 df_most = df_count[df_count > most_countries]
+print("Data fields that are present in most of the countries: ")
 print(df_most.index)
+print("\n")
 
 # In one line
 df_most_1 = df.groupby("WEO Subject Code")[2015].count()[lambda x: x > most_countries]
+print("Data fields that are present in most of the countries: ")
 print(df_most_1.index)
+print("\n")
 
 # Find Weo Subject Code descriptions
 df_desc = df[["WEO Subject Code", "Subject Descriptor"]].drop_duplicates()
+print("Data fields that are present in most of the countries with descriptions: ")
 print(df_desc[df_desc["WEO Subject Code"].isin(df_most.index)])
