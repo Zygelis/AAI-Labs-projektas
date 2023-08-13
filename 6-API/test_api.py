2a28c88244b4acde037289c6ec75177f28060f62
Žygimantas Girdauskas <zygiss.g@gmail.com>
2023-08-13 18:17:07 +0300

    Update to just test one country data at a time

--- 6-API/test_api.py
+++ 6-API/test_api.py
@@ -13,34 +13,26 @@ class Unittest(unittest.TestCase):
         with open("6-API\\test_data.json") as json_file:
             json_data = json.load(json_file)
 
-        # Iterate over the JSON data
-        for country in json_data:
-            # Get the JSON data for each country
-            json_data = country["features"]
-            target_GPD = country["target"]
+        # Get the JSON data
+        data = json_data[0]["features"]
 
-            # Send a POST request to the /predict_gdp endpoint
-            response = self.app.post(
-                "/predict_gdp",
-                data=json.dumps(json_data),
-                content_type="application/json",
-            )
+        # Send a POST request to the /predict_gdp endpoint
+        response = self.app.post(
+            "/predict_gdp",
+            data=json.dumps(json_data),
+            content_type="application/json",
+        )
 
-            # Check if the response status code is 200 (OK)
-            self.assertEqual(response.status_code, 200)
+        response = self.app.post("/predict_gdp", json=data)
+        data = response.get_json()
 
-            # Parse the JSON response data
-            data = json.loads(response.data)
+        # Check the response
+        self.assertEqual(response.status_code, 200)
 
-            # Check if the expected keys are present in the response
-            for key in target_GPD:
-                self.assertIn(key, data)
+        self.assertTrue("predicted_gdp_per_capita" in data)
 
-            # Check if the response contains the predicted_gdp_per_capita
-            self.assertIn("predicted_gdp_per_capita", data)
-
-            # Check if the predicted_gdp_per_capita value is a float
-            self.assertIsInstance(data["predicted_gdp_per_capita"][0], float)
+        # Check if the predicted_gdp_per_capita value is a float
+        self.assertIsInstance(data["predicted_gdp_per_capita"][0], float)
 
 
 if __name__ == "__main__":
